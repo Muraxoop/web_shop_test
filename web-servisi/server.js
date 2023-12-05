79a095921822501bf513772c1991f6b9d3d9c2f8
KarloFaks <karlohob@gmail.com>
2023-12-05 16:33:23 +0100

    d

--- web-servisi/server.js
+++ web-servisi/server.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/podaci", function (request, response) {
 
     return response.send("ok");
+
+});
+
+app.get("/podaci/:id", function (request, response) {
+
+    var id = request.params.id;
+    return response.send({message: id + 2 +" da"});
+
+});
+
+app.post("/podaci", function (request, response) {
+
+    var podaci = request.body.podatak;
+    return response.send({message: podaci +" da"});
 
 });
 
